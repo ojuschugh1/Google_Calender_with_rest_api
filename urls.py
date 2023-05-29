1a6d9b04b6606aa96ca2ce3f7aa6f08a34fae2e3
Ojus Chugh <79078267+ojuschugh1@users.noreply.github.com>
2023-05-29 20:36:59 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,8 +1,22 @@
-from django.urls import path
-from . import views
+"""tutorial URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('v1/calendar/init/', views.google_calendar_init_view, name='google_permission'),
-    path('v1/calendar/redirect/', views.google_calendar_redirect_view, name='google_redirect')
+    path('rest/', include('rest.urls')),
+    path('admin/', admin.site.urls),
 ]
